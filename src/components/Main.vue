c4350ae63aca1986c3647baab4bda4b33747f7eb
Emily C <echois03@gmail.com>
2021-05-17 14:49:52 -0700

    main component

---
+++ src/components/Main.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+<b-col>
+<b-row id="header">
+<img src="./assets/church.jpg">
+</b-row>
+<b-nav>
+  <b-nav-item>
+    <b-nav-text>Tab</b-nav-text>
+  </b-nav-item>
+</b-nav>
+</b-col>
+</template>
+
+<script>
+export default {}
+</script>
